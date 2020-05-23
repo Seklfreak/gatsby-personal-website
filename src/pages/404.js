52bc79afea53c1f7d284fc496c08785939803e03
Sebastian Winkler <sebastian@winkler.dev>
2020-05-23 22:56:07 +0200

    uses bare layout for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from "react"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import LayoutBare from "../components/layout-bare"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
+  <LayoutBare>
+    <SEO title="404: Not found"/>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  </LayoutBare>
 )
 
 export default NotFoundPage
